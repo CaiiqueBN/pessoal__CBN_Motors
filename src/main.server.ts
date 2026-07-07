24008023a3a1358f5129796b4498a0c671e75b35
CaiiqueBN <caiquebn.32@gmail.com>
2026-07-06 22:08:06 -0300

    Primeiro commit

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component'; // Ensure your import is correct
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+// Update the bootstrap function to accept 'context' and pass it to bootstrapApplication
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, config, context);
 
 export default bootstrap;
