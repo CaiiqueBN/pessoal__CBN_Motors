041d73d750db72ced29eba316884cb8ae446c6c6
CaiiqueBN <caiquebn.32@gmail.com>
2026-07-09 09:00:10 -0300

    Botão de Menu e Logout adicionados

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MenuComponent } from '../../components/menu/menu.component';
+import { Router } from '@angular/router';
+import { WelcomeComponent } from '../../components/welcome/welcome.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [WelcomeComponent, MenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  router = inject(Router);
 
+  logout() {
+    sessionStorage.clear();
+    this.router.navigate([""]);
+  }
 }
